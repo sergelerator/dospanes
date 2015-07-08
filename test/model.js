64286efe29429695110375d50800016aed1ed989
Sergio Morales <i.serge23@gmail.com>
2015-07-08 10:11:40 -0500

    _hidden properties and tests for Model.build

--- test/model.js
+++ test/model.js
@@ -23,7 +23,7 @@ describe('Model', function(){
       modelName = 'User';
     });
 
-    beforeEach(function(){
+    afterEach(function(){
       if (Model[modelName]) {
         delete Model[modelName];
       }
@@ -84,6 +84,82 @@ describe('Model', function(){
 
       it('has an empty store', function(){
         expect(user.store).to.deep.equal([]);
+      });
+    });
+  });
+
+  describe('with model name "Repeated"', function(){
+    var modelName, modelDescription, model;
+
+    before(function(){
+      modelName = 'Repeated';
+      modelDescription = {};
+    });
+
+    beforeEach(function(){
+      model = Model(modelName, modelDescription);
+    });
+
+    it('returns a reference to the object stored as a property of "Model"', function(){
+      expect(model).to.equal(Model[modelName]);
+    });
+
+    it('returns the same object if called twice', function(){
+      var secondCall = Model(modelName, {});
+      expect(model).to.equal(secondCall);
+    });
+  });
+});
+
+describe('model.build', function(){
+  var modelName, modelDescription, model, attributes;
+
+  describe('"User" model', function(){
+    var user;
+
+    before(function(){
+      modelName = 'User';
+      modelDescription = {
+        attributes: {
+          firstName: Attribute.text,
+          lastName: Attribute.text,
+
+          fullName: Attribute.computed(function(){
+            return this.firstName + ' ' + this.lastName;
+          })
+        }
+      };
+      model = Model(modelName, modelDescription);
+    });
+
+    after(function(){
+      if (Model[modelName]) {
+        delete Model[modelName];
+      }
+    });
+
+    describe('with attributes param', function(){
+      var firstName, lastName;
+
+      before(function(){
+        firstName = 'Tyrion', lastName = 'Lannister';
+        attributes = {
+          firstName: firstName,
+          lastName: lastName
+        };
+        user = model.build(attributes);
+      });
+
+      it('has a firstName property equal to "Tyrion"', function(){
+        expect(user).to.have.property('firstName').and.equal(firstName);
+      });
+
+      it('has a lastName property equal to "Lannister"', function(){
+        expect(user).to.have.property('lastName').and.equal(lastName);
+      });
+
+      it('has a fullName property equal to "Tyrion Lannister"', function(){
+        expect(user).to.have.property('fullName').and.equal('Tyrion Lannister');
       });
     });
   });
